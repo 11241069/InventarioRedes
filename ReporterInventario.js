182d8efae48bfdac89553e158ebe860f3ce7623d
11241069 <carlos.pagoaga@unitec.edu>
2015-11-30 10:39:01 -0600

    gg

--- ReporterInventario.js
+++ ReporterInventario.js
@@ -1,13 +1,19 @@
 angular.module('myApp').controller('reportes_inventario', ['$scope','$http','$location',function ($scope,$http,$location) {
-
+"http://fia.unitec.edu:8082/InventarioRedes/phpFiles/getInventory.php"
     var ctrl = this;
-    $
-    $scope.atributosBusqueda = [];
-    
-    
+    $scope.allOptions = [];
+    $scope.selectedOption = {};
+
     ctrl.init = function(){
-        $.get("reporteinventario.php");
-        
+        var baseUrl = "http://fia.unitec.edu:8082/InventarioRedes/phpFiles/getInventory.php";
+        var request = {
+                method: 'GET',
+                url: baseUrl
+        };
+        $http(request).then(function(response){
+            $scope.allOptions = response.data;
+            console.log(response.data);
+        });
     };
     ctrl.init();
 
--- reporteinventario.php
+++ reporteinventario.php
@@ -1,14 +1,4 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "daemonaeon";
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
 echo "Connected successfully";
 ?> 
