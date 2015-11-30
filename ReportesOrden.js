10852124d7a3f314c86f97c4839844405a9e5587
11241069 <carlos.pagoaga@unitec.edu>
2015-11-30 11:12:03 -0600

    Inventario Orders

---
+++ ReportesOrden.js
@@ -0,0 +1,20 @@
+angular.module('myApp').controller('reportes_orden', ['$scope','$http','$location',function ($scope,$http,$location) {
+"http://fia.unitec.edu:8082/InventarioRedes/phpFiles/getInventory.php"
+    var ctrl = this;
+    $scope.Orders = [];
+    
+
+    ctrl.init = function(){
+        var baseUrl = "http://fia.unitec.edu:8082/InventarioRedes/phpFiles/getOrders.php";
+        var request = {
+                method: 'GET',
+                url: baseUrl
+        };
+        $http(request).then(function(response){
+            $scope.Orders = response.data;
+            console.log(response.data);
+        });
+    };
+    ctrl.init();
+
+}]);
