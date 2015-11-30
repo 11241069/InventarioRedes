e685a5e80e3a5d9a522abebdfbd0452e5fb62c3f
11241069 <carlos.pagoaga@unitec.edu>
2015-11-30 16:18:40 -0600

    gg

---
+++ crearexpediente.js
@@ -0,0 +1,21 @@
+angular.module('myApp').controller('crearproducto', ['$scope','$http','$location',function ($scope,$http,$location) {
+    $scope.submit = function () {
+    
+        var path = $location.path($location.path());
+		//Creating the baseUrl
+		var baseUrl = "http://fia.unitec.edu:8082/InventarioRedes/phpFiles/insertProducto.php"
+';
+        
+var request = {
+			method: 'POST',
+			url: baseUrl,
+			data: {product_barcode: $scope.barcode,product_name: $scope.nombre, product_category: $scope.categoria, purchase_price: $scope.purch,sell_price:$scope.sell}
+	};
+	
+	$http(request).then(function(response){
+        alert("agregado");
+	});
+         
+ 
+};
+}]);
